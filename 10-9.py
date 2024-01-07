92528c9618d5ec2fdcad2e643698405547005f00
tintoll <belselios@gmail.com>
2024-01-08 05:37:35 +0900

    14-3

--- 10-9.py
+++ 10-9.py
@@ -1,19 +1,20 @@
 import pyupbit
 
 df = pyupbit.get_ohlcv("KRW-BTC")
-print(df.tail()) # 마지막 행 5개 가져오기
-
-df["종가하나내린거"] = df["close"].shift(1)
-print(df.head()) # 상위 행 5개 가져오기
-
-print(df)
-# 추가
-df['diff'] = df['high'] - df['low']
-print(df)
-
-# 삭제
-df2 = df.drop('diff', axis=1) # axis=1이여야지 컬럼 삭제, 0이면 행삭제
-print(df2)
+df.to_excel('btc.xlsx')
+# print(df.tail()) # 마지막 행 5개 가져오기
+#
+# df["종가하나내린거"] = df["close"].shift(1)
+# print(df.head()) # 상위 행 5개 가져오기
+#
+# print(df)
+# # 추가
+# df['diff'] = df['high'] - df['low']
+# print(df)
+#
+# # 삭제
+# df2 = df.drop('diff', axis=1) # axis=1이여야지 컬럼 삭제, 0이면 행삭제
+# print(df2)
 # import pandas as pd
 #
 # data = "2021-01-01"
